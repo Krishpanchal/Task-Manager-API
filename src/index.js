87168295b83eda5f4ec83f7b7de4a09e47fccc34
Krishpanchal <krishpanchal45@gmail.com>
2021-01-15 12:26:47 +0530

    Created post routes for users and tasks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+require("./db/mongoose") //Runs this file first to connect the database
+const User = require("./models/user")
+const Task = require("./models/tasks")
 
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json()) //This will automatically parse the upcoming json into JS Object so that we can use it.
+
+app.post("/users", (req, res) => {
+  const user = new User(req.body)
+
+  user.save().then(() => {
+      res.status(201).send(user)
+  }).catch((e) => {
+      res.status(400).send(e)
+  })
+});
+
+app.post("/tasks" , (req, res)=>{
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+});
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`)
 });
